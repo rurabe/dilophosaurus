c192d87c63d7004db2cf036c64c7e5c5b1c4f3cf
Ryan Urabe <rurabe@gmail.com>
2015-03-18 09:12:29 -0700

    got it working i thinks

---
+++ models/c.rb
@@ -0,0 +1,51 @@
+class C
+  attr_reader :subnet, :ticketmaster, :proxy, :axs
+  def initialize(subnet,a,b,c,mask)
+    @subnet = subnet
+    @a = a
+    @b = b
+    @c = c
+    @mask = mask
+  end
+
+  def generate_ips!(i)
+    @ticketmaster = ticketmaster_ips(i)
+    @proxy = proxy_ips(i)
+    @axs = @proxy.sample(6)
+  end
+
+  private
+
+    def ticketmaster_ips(i)
+      ticketmaster_ds(i).map do |d|
+        output(d)
+      end
+    end
+
+    def proxy_ips(i)
+      proxy_ds(i).map do |d|
+        output(d)
+      end
+    end
+
+    def ticketmaster_ds(i)
+      case i % 2
+      when 0 then [4, 6, 8, 11, 19, 20, 23, 27, 29, 31, 33, 38, 40, 42, 45, 47, 49, 51, 52]
+      when 1 then [2, 3, 5, 7, 9, 15, 17, 21, 25, 30, 32, 34, 35, 37, 39, 41, 46, 48, 50]
+      end
+    end
+
+    def proxy_ds(i)
+      case i % 4
+      when 0 then [58, 61, 64, 66, 69, 71, 73, 76, 79, 82, 84, 87, 89, 92, 95, 97, 100, 102, 104]
+      when 1 then [106, 108, 111, 113, 116, 118, 121, 124, 126, 129, 132, 134, 137, 139, 142, 145, 147, 149, 151]
+      when 2 then [153, 156, 158, 160, 162, 165, 168, 171, 173, 176, 179, 181, 183, 186, 189, 192, 194, 197, 199]
+      when 3 then [201, 203, 206, 209, 212, 215, 218, 221, 224, 226, 228, 230, 233, 235, 238, 240, 242, 244, 246]
+      end
+    end
+
+    def output(d)
+      [[@a,@b,@c,d].join("."),@mask].join(",")
+    end
+
+end
--- web.rb
+++ web.rb
@@ -7,6 +7,10 @@ get '/' do
 end
 
 post '/split' do
-  @data = params[:ips].split(/\n/).map{|i| Subnet.new(i) }
+  @subnets = params[:ips].split(/\n/).map{|i| Subnet.new(i) }
+  @cs = @subnets.flat_map{|s| s.cs }.each_with_index {|c,i| c.generate_ips!(i) }
+  @tm = @cs.flat_map(&:ticketmaster).shuffle
+  @px = @cs.flat_map(&:proxy).shuffle
+  @axs = @cs.flat_map(&:axs).shuffle
   slim :split
 end
